8353ac6d8604c2abd186ec295502e874ed152d29
Ghost <ghost@Ghosts-MBP.hsd1.ca.comcast.net>
2019-05-29 20:43:38 -0700

    created table products SQL

--- bamazon.sql
+++ bamazon.sql
@@ -1,3 +1,15 @@
+DROP DATABASE IF EXISTS bamazon_db;
+-- Create a MqSQL Database called 'bamazon' --
 create database bamazon_db;
 
 USE bamazon_db;
+-- Create a table inside of that database called 'products' --
+
+CREATE TABLE products (
+item_id INT(11) AUTO_INCREMENT NOT NULL,
+product_name VARCHAR(30) NULL,
+department_name VARCHAR(50) NULL,
+price DECIMAL(10,2) NULL,
+stock_quantity INT(11) NOT NULL,
+PRIMARY KEY (item_id)
+);
