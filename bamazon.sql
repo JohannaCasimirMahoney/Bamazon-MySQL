4c96583f30f6afc0d5c09169392bec7b8ed3f213
Ghost <ghost@Ghosts-MBP.hsd1.ca.comcast.net>
2019-06-02 12:29:33 -0700

    fixed comma in values section

--- bamazon.sql
+++ bamazon.sql
@@ -17,12 +17,16 @@ PRIMARY KEY (item_id)
 -- Populate database with around 10 different products --
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUE ("Brake Pads", "Brakes and Traction Control", 99.00, 300),
+VALUES ("Brake Pads", "Brakes and Traction Control", 99.00, 300),
       ("Floor Mat", "Floor Mats and Carpet Products", 1.99, 50),
       ("Wheel Nut", "Tire Repair", 89, 100),
-      ("Oil Filter Wrench," "Shop and Garage Tools", 6.99, 40),
+      ("Oil Filter Wrench", "Shop and Garage Tools", 6.99, 40),
       ("Brake Caliper-Front", "Brakes and Traction Control", 65.99,30),
       ("Spark Plug", "Ignition", 7.49, 60),
+      ("Alternator", "Starter and Alternator", 176.99, 45),
+      ("Trailer Hitch","Truck and Towing", 157.99, 100),
+      ("Gear Shift Knob", "Interior Accessories", 36.99, 25),
+      ("Battery", "Ignition Up and Routine Maintenance", 199.99, 200);
       
       
       
