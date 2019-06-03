3c26685a8bc5568afac11316bcbc31a00a8179a2
Ghost <ghost@Ghosts-MBP.hsd1.ca.comcast.net>
2019-06-02 18:26:52 -0700

    added connection code and console log

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,14 +22,6 @@ connection.connect(function (err) {
     console.log("connected as id" + connection.threadId + "\n");
 });
 
-function afterConnection() {
-    connection.query("SELECT * FROM products", function (err, res) {
-        if (err) throw err;
-        console.log(res);
-        connection.end();
-    });
-}
-
 // This function will prompt the user for what action they should take
 function start() {
     inquirer.prompt([
@@ -43,5 +35,53 @@ function start() {
             type: "input",
             message: "How many units of the product would you like to buy?",
         }
-    ])
+    ]).then(function (res) {
+        connection.query(
+            "SELECT * FROM products",
+            {
+                item_id: parseInt(res.id)
+            },
+            function (err, pRes) {
+                if (err) console.log(err);
+                console.log("Product ID" + res.id);
+                console.log("Amount: " + res.id);
+                // If user orders too many of the same product, this message below will display
+                if (parseInt(res.product) > pRes[0].stock_quantity) {
+                    console.log("Insufficient Quantity!");
+                    connection.end();
+                }
+                // This updates the database values to reduce the stock_quantity
+                else {
+                    connection.query(
+                        "UPDATE products set?"
+                        [
+                        {
+                            stock_quantity: pRes[0].stock_quantity - parseInt(res.product)
+                        },
+                        {
+                            item_id: parseInt(res.id)
+                        }
+                        ]
+                    );
+                    // This will update row and display the price and remaining stock
+                    connection.query(
+                        "SELECT * FROM products",
+                        {
+                            item_id: parseInt(res.id)
+                        },
+                        // This is for the updated row
+                        function (err, sRes) {
+                            if (err) console.log(err);
+                            var totalPrice = pRes[0].price * parseInt(res.product);
+                            console.log("Your price is " + totalPrice);
+                            console.log("Stock remaining: " + sRes[0].stock_quantity);
+                        }
+                    );
+                    connection.end();
+                };
+            }
+        )
+    })
 }
+
+
