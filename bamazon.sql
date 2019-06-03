cfb6d01859db11e235ef28862108e0ac939a5cdf
Ghost <ghost@Ghosts-MBP.hsd1.ca.comcast.net>
2019-06-02 18:32:13 -0700

    updated commentout by adding rows

--- bamazon.sql
+++ bamazon.sql
@@ -14,13 +14,13 @@ price DECIMAL(10,2) NULL,
 stock_quantity INT(11) NOT NULL,
 PRIMARY KEY (item_id)
 );
--- Populate database with around 10 different products --
+-- created rows that shows the database with around 10 different products --
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Brake Pads", "Brakes and Traction Control", 99.00, 300),
       ("Floor Mat", "Floor Mats and Carpet Products", 1.99, 50),
       ("Wheel Nut", "Tire Repair", 89, 100),
-      ("Oil Filter Wrench", "Shop and Garage Tools", 6.99, 40),
+      ("Oil Filter Wrench",products "Shop and Garage Tools", 6.99, 40),
       ("Brake Caliper-Front", "Brakes and Traction Control", 65.99,30),
       ("Spark Plug", "Ignition", 7.49, 60),
       ("Alternator", "Starter and Alternator", 176.99, 45),
