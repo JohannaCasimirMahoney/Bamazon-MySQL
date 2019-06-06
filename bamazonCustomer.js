76222916935a47f3684ad8a0cabe437b080cc8be
Ghost <ghost@Ghosts-MBP.hsd1.ca.comcast.net>
2019-06-05 21:23:13 -0700

    code does not display the ID info. None working code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-// Creating the connection information for the sql database
+// Initializes the connection for the sql database
 var connection = mysql.createConnection({
     host: "localhost",
     // Port number
@@ -13,80 +13,52 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-connection.connect(function (error) {
-    if (error) throw error;
-
-    var query = connection.query(
-        "SELECT * FROM products",
-        function (err, data) {
-            for (var i = 0; i < data.length; i++) {
-                console.log("ID: " + data[i].id + " PRODUCT: " + data[i].name + " $" + data[i].price);
-            }
-            inquirer.prompt([
-                {
-                    name: "itemId",
-                    message: "Enter the ID of the product you would like to buy."
-                },
-                {
-                    name: "quantity",
-                    message: "How many units of the product would you like to buy?"
-                }
-            ]).then(function (answers) {
-                var query = connection.query(
-                    "SELECT * FROM products",
-                    {
-                        id: answers.itemId
-                    },
-                    function (err, data) {
-                        if (answers.quantity > data[0].stock) {
-                            console.log("Insufficient Quantity!");
-                            connection.end();
-                        } else {
-                            var query = connection.query(
-                                "UPDATE products SET",
-                                [
-                                    {
-                                        stock: data[0].stock - answers.quantity,
-                                        sales: data[0].sales + (data[0].price * answers.quantity)
-                                    },
-                                    {
-                                        id: answers.itemId
-                                    }
-                                ],
-                                function (err, data) {
-                                    console.log("our Order Is Placed!");
-                                    connection.end();
-                                }
-                            );
-                        }
-                    }
-                );
-            });
+var query = connection.query(
+    "SELECT * FROM products",
+    function (err, data) {
+        for (var i = 0; i < data.length; i++) {
+            console.log("ID: " + data[i].id + " PRODUCTS: " + data[i].name + " $" + data[i].price);
         }
-    );
-});
+        inquirer.prompt([
+            {
+                name: "item_id",
+                message: "Enter the ID of the product you would like to buy."
+            },
+            {
+                name: "quantity",
+                message: "How many units of the product would you like to buy?"
+            }
+        ]).then(function (answers) {
+            var query = connection.query(
+                "SELECT * FROM products",
+                {
+                    id: answers.item_id
+                },
+                function (err, data) {
+                    if (answers.quantity > data[0].stock) {
+                        console.log("Insufficient Quantity!");
+                        connection.end();
+                    } else {
+                        var query = connection.query(
+                            "UPDATE products SET",
+                            [
+                                {
+                                    stock: data[0].stock - answers.quantity,
+                                    sales: data[0].sales + (data[0].price * answers.quantity)
+                                },
+                                {
+                                    id: answers.item_id
+                                }
+                            ],
+                            function (err, data) {
+                                console.log("Your Order Is Placed!");
+                                connection.end();
+                            }
+                        );
+                    }
+                }
+            );
+        });
+    }
+);
+
